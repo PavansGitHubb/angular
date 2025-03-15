f1edf3ad0a71ced2069036017a060be4c13c23b3
Pavan kumar <136694590+PavansGitHubb@users.noreply.github.com>
2025-03-15 13:10:26 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 set -e
 
-# Move Angular build files to the correct location
-sudo mv /var/www/html/browser/* /var/www/html/
-sudo rm -rf /var/www/html/browser
-
-# Set correct ownership and permissions
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 755 /var/www/html
-sudo find /var/www/html -type f -exec chmod 644 {} \;
-
-# Remove default Nginx config
-sudo rm -f /etc/nginx/sites-enabled/default
-
-# Add new Nginx config for Angular
-cat <<EOF | sudo tee /etc/nginx/sites-available/angular
-server {
-    listen 80;
-    server_name _;
-
-    root /var/www/html;
-    index index.html;
-
-    location / {
-        try_files \$uri /index.html;
-    }
-
-    error_page 404 =200 /index.html;
-}
-EOF
-
-# Enable the new config
-sudo ln -s /etc/nginx/sites-available/angular /etc/nginx/sites-enabled/angular
-sudo rm -f /etc/nginx/sites-enabled/default  # Remove old default config
-
 # Restart Nginx
 sudo systemctl restart nginx
